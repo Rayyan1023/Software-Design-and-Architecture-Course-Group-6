423d6ac204a29618eaf8a6a11748aa92691c57fc
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-09-20 18:58:40 -0400

    Update HoodieTest.java

--- Assignment1/SRC/HoodieTest.java
+++ Assignment1/SRC/HoodieTest.java
@@ -7,18 +7,18 @@ class HoodieTest {
     private double hoodiePrice;
     @BeforeEach
     void setUp(){
-        // Creates new json object, that gets the price of table from the json file
+      
         JSON jsonHoodie = new JSON("hoodie");
         hoodiePrice = jsonHoodie.getJson();
     }
 
     @Test
     void setPrice() {
-        // Creates new a classic table object, then sets the price that is based on the
+       
         ProductFactory clothesFactory = FactoryProducer.getFactory(false);
         Clothes hoodie = clothesFactory.getClothes("HOODIE");
 
-        // Compares the prices of the JSON file to that of the actual price
-        assertEquals(100.00, hoodiePrice, 0);
+        
+        assertEquals(100.00, Price, 0);
     }
 }
