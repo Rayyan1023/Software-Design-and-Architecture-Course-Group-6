536a3ffe0a7a943e6a4014470d7446f0c845fd12
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 16:26:42 -0400

    Create Price.java

---
+++ assignment1/src/Price.java
@@ -0,0 +1,13 @@
+package com.company;
+
+// Declares prices for the shoe types
+public enum Price {
+    // Dress Shoes
+    $100,
+    // High Heels
+    $80,
+    // Running Shoes
+    $40,
+    // Slippers
+    $20
+}
