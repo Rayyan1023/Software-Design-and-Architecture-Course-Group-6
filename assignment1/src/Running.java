6eb2afe2ba35ed4a7ec35350209b52cda2d26aa7
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 16:26:54 -0400

    Update Running.java

--- assignment1/src/Running.java
+++ assignment1/src/Running.java
@@ -1,5 +1,6 @@
 package com.company;
 
+// Creating class for Running shoes and extending it to the Shoe Class
 class Running extends Shoe {
     Running(Price price) {
         super(ShoeType.RUNNING, price);
