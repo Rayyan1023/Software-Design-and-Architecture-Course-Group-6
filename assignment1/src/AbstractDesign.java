279bd4533563c548150f0199e76e20e562d377ea
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 16:25:38 -0400

    Update AbstractDesign.java

--- assignment1/src/AbstractDesign.java
+++ assignment1/src/AbstractDesign.java
@@ -3,7 +3,7 @@ package com.company;
 class AbstractDesign {
     public static void main(String[] args) {
 
-        // Outputs a
+        // Outputs all shoe types and their respective prices
         System.out.println(ShoeFactory.buyShoe(ShoeType.RUNNING));
         System.out.println(ExpensiveShoesFactory.buyShoe(ShoeType.HIGHHEELS));
         System.out.println(ExpensiveShoesFactory.buyShoe(ShoeType.DRESS));
