a3bc4d127093491d3b43cf27f32d50f719d26def
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 16:27:52 -0400

    Update Slippers.java

--- assignment1/src/Slippers.java
+++ assignment1/src/Slippers.java
@@ -1,5 +1,6 @@
 package com.company;
 
+// Creating class for Slippers and extending it to the Shoe Class
 class Slippers extends Shoe {
     Slippers(Price price) {
         super(ShoeType.SLIPPERS, price);
