2bcd7f2b42332d13423dfa678cdf48756951845c
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 16:17:42 -0400

    Create Slippers.java

---
+++ assignment1/src/Slippers.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class Slippers extends Shoe {
+    Slippers(Price price) {
+        super(ShoeType.SLIPPERS, price);
+        construct();
+    }
+
+    @Override
+    protected void construct () {
+
+    }
+}
