1043971b4563d7f2cbd61779e5ca9a5a2b4a4079
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 16:27:08 -0400

    Update Shoe.java

--- assignment1/src/Shoe.java
+++ assignment1/src/Shoe.java
@@ -2,6 +2,7 @@ package com.company;
 
 abstract class Shoe {
     Shoe(ShoeType model, Price price) {
+
         this.model = model;
         this.price = price;
     }
@@ -10,6 +11,7 @@ abstract class Shoe {
     ShoeType model = null;
     Price price = null;
 
+    // Fetches SHoe type and returns it
     ShoeType getModel() {
         return model;
     }
@@ -17,12 +19,12 @@ abstract class Shoe {
     void setModel(ShoeType model) {
         this.model = model;
     }
-
+     // Fetches Shoe price and returns it
     Price getPrice() {
         return price;
     }
 
-
+    // Output
     @Override
     public String toString() {
         return "Shoe Type - " + model + " price is: " +price;
