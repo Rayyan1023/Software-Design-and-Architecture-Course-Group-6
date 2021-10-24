aa4057ec7cbab86e637e31e990c0b25d3fa5632a
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 16:16:50 -0400

    Create HighHeels.java

---
+++ assignment1/src/HighHeels.java
@@ -0,0 +1,14 @@
+package com.company;
+
+class HighHeels extends Shoe {
+    HighHeels(Price price) {
+        super(ShoeType.HIGHHEELS, price);
+        construct();
+    }
+
+    @Override
+    protected void construct () {
+
+    }
+}
+
