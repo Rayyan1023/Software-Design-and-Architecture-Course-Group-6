282491f7cd6ff2e0a0928510e5f9f6a729cd1633
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 16:17:17 -0400

    Create Running.java

---
+++ assignment1/src/Running.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class Running extends Shoe {
+    Running(Price price) {
+        super(ShoeType.RUNNING, price);
+        construct();
+    }
+
+    @Override
+    protected void construct () {
+
+    }
+}
